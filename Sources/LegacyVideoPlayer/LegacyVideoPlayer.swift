5b99bfd1ddabb5e6bc339e647c4fa800e305b8d5
bwide <bfwide07@gmail.com>
2022-02-25 11:01:21 -0300

    Initial Commit

---
+++ Sources/LegacyVideoPlayer/LegacyVideoPlayer.swift
@@ -0,0 +1,6 @@
+public struct LegacyVideoPlayer {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/LegacyVideoPlayerTests/LegacyVideoPlayerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import LegacyVideoPlayer
+
+final class LegacyVideoPlayerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(LegacyVideoPlayer().text, "Hello, World!")
+    }
+}
